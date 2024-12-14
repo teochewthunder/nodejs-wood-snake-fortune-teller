2099b263e40e20e028680f1c83352d203f8feb82
Roger Tan <teochewthunder@gmail.com>
2024-12-14 22:48:32 +0800

    Background image

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("view engine", "handlebars");
 app.set("port", process.env.PORT || 3000);
 
 app.use(require("body-parser")());
+app.use(express.static("img"));
 
 app.get("/", (req, res)=> {
 	res.render("form", { error: "test", fortune: "To get your fortune in the year 2025, please provide your birth date." });
@@ -53,11 +54,11 @@ app.post("/fortune", (req, res)=> {
 		res.render("form", { error: err, fortune: "" });
 	});
 });
-
+/*
 app.use((req, res, next)=> {
 	res.status(404);
 	res.render("404");
-});
+});*/
 
 app.use((err, req, res, next)=> {
 	res.status(500);
